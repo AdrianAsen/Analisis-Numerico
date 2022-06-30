33b343e2731e974c64c6123ea476ed57fc899b0a
Enriqueacs19 <69475362+Enriqueacs19@users.noreply.github.com>
2022-06-30 16:21:16 -0500

    Update muller.m

--- Funciones/muller.m
+++ Funciones/muller.m
@@ -47,7 +47,7 @@ while(i<=N0)
     Y=[f(p0),f(p2),f(p1)];
     plot(X,Y,'bo-','markersize',5);
     fplot(fx,'k-','LineWidth',2);%Grafica la función de color negro y grosor 2
-    title('Método de la bisección'); hold on; grid on; %Título de la función.
+    title('Método de Müller'); hold on; grid on; %Título de la función.
     axis([-10 10 -10  10])
     line([-100 100],[0 0],'Color','k','LineStyle','--'); %Marca el eje X.
     line([0 0],[-100 100],'Color','k','LineStyle','--'); %Marca el eje Y.
